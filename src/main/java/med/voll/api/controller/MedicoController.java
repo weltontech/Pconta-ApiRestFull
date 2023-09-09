482abba29de6ae2c20f62bf894c0241c2500ebc0
weltontech <weltonjc1@hotmail.com>
2023-09-09 16:36:48 -0300

    Criado metodo atualizar para medicos; criado DTO DadosAtualizacaoMedico; criacao do metodo atualizarInformacoes dentro da Classe Medico; criação metodo atualizarInformacao dentro da classe Endereco.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -9,11 +9,13 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
@@ -37,5 +39,15 @@ public class MedicoController {
 	public Page<DadosListagemMedico> listar(@PageableDefault(size=10, sort= {"nome"})  Pageable paginacao){
 		return repository.findAll(paginacao).map(DadosListagemMedico::new);
 	}
-}
+	
+	@PutMapping
+	@Transactional
+	public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+		Medico medico  =  repository.getReferenceById(dados.id());
+		medico.atualizarInformacoes(dados);
+		//Obs: a transação é feita automaticamente pelo transactional. 
+	}
+	}
+	
+
 /** @Pageable --> é uma anotação para Pageable, nele voce conseguira configurar a paginação**/
