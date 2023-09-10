46250c8c46a02e306cb65b09c971909c22d9f820
weltontech <weltonjc1@hotmail.com>
2023-09-10 14:14:18 -0300

    ControllerMedico adicionado metodo excluir fisico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -47,6 +49,12 @@ public class MedicoController {
 		Medico medico  =  repository.getReferenceById(dados.id());
 		medico.atualizarInformacoes(dados);
 		//Obs: a transação é feita automaticamente pelo transactional. 
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void excluir(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 	}
 	
